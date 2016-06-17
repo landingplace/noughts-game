5dc37e5eb1472d5b397c2b12ea490dcc87e8b529
landingplace <liji.de@gmail.com>
2016-06-17 10:17:12 -0400

    Create noughts-play.py
    this program implement the noughts game for two people to compete with each other

---
+++ noughts-play.py
@@ -0,0 +1,14 @@
+# A simple, Noughts and Crosses game for two human players implemented using
+# the PyGame framework. Created by Liam Fraser for a Linux User and Developer
+# tutorial.
+
+# Provides the PyGame framework
+import pygame 
+
+# Provides the sys.exit function we use to exit our game loop
+import sys 
+from pygame import *
+
+# Import constants used by PyGame
+from pygame.locals import * 
+
